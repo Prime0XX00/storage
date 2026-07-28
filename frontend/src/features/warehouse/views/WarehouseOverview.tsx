4f99bd7acfbd9d8488e8e825e7f6b7821d5fd111
Prime0XX00 <1max.thiede@gmail.com>
2026-07-28 13:59:41 +0200

    basic data display for warehouses

--- frontend/src/features/warehouse/views/WarehouseOverview.tsx
+++ frontend/src/features/warehouse/views/WarehouseOverview.tsx
@@ -1,5 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+
 const WarehouseOverview = (): React.JSX.Element => {
-	return <>Warehouse</>;
+	const query = useQuery({
+		queryKey: ["warehouses"],
+		queryFn: async () => {
+			const res = await fetch("http://localhost:3000/api/warehouse/");
+			const json = await res.json();
+			return json;
+		},
+	});
+
+	return <>{JSON.stringify(query.data)}</>;
 };
 
 export default WarehouseOverview;
