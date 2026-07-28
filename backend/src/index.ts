ebdeb51b1b87114125f644cd293a570bb34a2216
Prime0XX00 <1max.thiede@gmail.com>
2026-07-28 13:59:06 +0200

    cors express setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express, { Request, Response, Application } from "express";
 import { warehouseRouter } from "./features/warehouse/routes";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/warehouse", warehouseRouter);
