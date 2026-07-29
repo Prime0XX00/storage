a4581db7872c3902ed80b91b20beedd853cb4f87
Prime0XX00 <1max.thiede@gmail.com>
2026-07-29 11:08:25 +0200

    added basic post endpoint for warehouses

--- backend/src/features/warehouse/controllers.ts
+++ backend/src/features/warehouse/controllers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { db } from "../../connection";
+import { eq } from "drizzle-orm";
 import { warehousesTable } from "../../db/schema";
 
 export const getAllWarehouses = async (
@@ -9,4 +10,33 @@ export const getAllWarehouses = async (
 ) => {
 	const warehouses = await db.select().from(warehousesTable);
 	res.json(warehouses);
+};
+
+export const postNewWarehouse = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	const body = req.body;
+
+	const { name, code } = body;
+
+	if (!name) throw new Error("Parameter 'name' is missing.");
+	if (!code) throw new Error("Parameter 'code' is missing.");
+
+	const existingWarehouse = await db
+		.select()
+		.from(warehousesTable)
+		.where(eq(warehousesTable.code, code));
+
+	if (existingWarehouse)
+		throw new Error(`Warehouse with code '${code}' already exists.`);
+
+	const insertedWarehouse = await db
+		.insert(warehousesTable)
+		.values({ name, code })
+		.returning()
+		.onConflictDoNothing();
+
+	res.json(insertedWarehouse);
 };
